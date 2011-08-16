a3012d439b030a3c4f1b557e37301333989a9275
Roman Divacky <rdivacky@freebsd.org>
2011-08-16 12:09:40 +0200

    Fix abbrev generation.

--- libdwarf_abbrev.c
+++ libdwarf_abbrev.c
@@ -222,7 +222,7 @@ int
 _dwarf_abbrev_gen(Dwarf_P_Debug dbg, Dwarf_Error *error)
 {
 	Dwarf_CU cu;
-	Dwarf_Abbrev ab;
+	Dwarf_Abbrev ab, tab;
 	Dwarf_AttrDef ad;
 	Dwarf_P_Section ds;
 	int ret;
@@ -236,7 +236,7 @@ _dwarf_abbrev_gen(Dwarf_P_Debug dbg, Dwarf_Error *error)
 	    DW_DLE_NONE)
 		return (ret);
 
-	for (ab = cu->cu_abbrev_hash; ab != NULL; ab = ab->ab_hh.next) {
+        HASH_ITER(ab_hh, cu->cu_abbrev_hash, ab, tab) {
 		RCHECK(WRITE_ULEB128(ab->ab_entry));
 		RCHECK(WRITE_ULEB128(ab->ab_tag));
 		RCHECK(WRITE_VALUE(ab->ab_children, 1));
